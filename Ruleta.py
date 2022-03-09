90375b40d34c90a1f5f50bd1a4d2736a027da612
Daniel Guerrero <danielgdevil@gmail.com>
2022-03-09 16:56:05 -0500

    Mayor_Corrections

--- Ruleta.py
+++
@@ -1,6 +0,0 @@
-import random
-
-testcase = [5, 12, 25, 38]
-
-print(random.randrange(38))
-
--- population.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def generator(instruction_set):
-    chromosome = []
-    for i in range(15):
-        chromosome.append(instruction_set[random.randrange(4)])
-    return str(chromosome)
-
